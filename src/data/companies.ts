ecf1893b09985b3f0d79d27d91d769209903c337
Raiyan Hossain <ryanhossain9797@protonmail.com>
2026-01-13 17:03:52 +0600

    Use version-less Gist URL

--- src/data/companies.ts
+++ src/data/companies.ts
@@ -1,6 +1,7 @@
 import type { Company, Tag } from '../types/company'
 
-export const DATA_URL = 'https://gist.githubusercontent.com/ryanhossain9797/8e53fb3e3e0812ec499ac5caeb9b642f/raw/9e972faecff4b979a52ad6fea3bbf4b7987b6060/career_page_data.json'
+// Using the version-less URL for the gist to ensure we always get the latest
+export const DATA_URL = 'https://gist.githubusercontent.com/ryanhossain9797/8e53fb3e3e0812ec499ac5caeb9b642f/raw/career_page_data.json'
 
 export interface PageData {
     tags: Tag[]
