32d330c6b08c391300968317128adc8cc140ec15
Raiyan Hossain <ryanhossain9797@protonmail.com>
2026-01-19 17:29:15 +0600

    Redirect to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Use '/' on Vercel, but keep '/career_pages/' for GitHub Pages
-  base: process.env.VERCEL ? '/' : '/career_pages/',
+  base: '/',
 })
